faecfbe2230f26ef78b47e140c3b31c214b525c7
TheOmnipresence <ItsTheOmnipresence@gmail.com>
2026-06-05 18:38:45 -0400

    progress

--- locations.py
+++ locations.py
@@ -7,7 +7,7 @@ from BaseClasses import ItemClassification, Location
 from . import items
 
 if TYPE_CHECKING:
-    from .world import APQuestWorld
+    from .world import SweetDefeatWorld
 
 # Every location must have a unique integer ID associated with it.
 # We will have a lookup from location name to ID here that, in world.py, we will import and bind to the world class.
@@ -26,8 +26,8 @@ LOCATION_NAME_TO_ID = {
 
 # Each Location instance must correctly report the "game" it belongs to.
 # To make this simple, it is common practice to subclass the basic Location class and override the "game" field.
-class APQuestLocation(Location):
-    game = "APQuest"
+class SweetDefeatLocation(Location):
+    game = "Sweet Defeat"
 
 
 # Let's make one more helper method before we begin actually creating locations.
@@ -40,12 +40,12 @@ def get_location_names_with_ids(location_names: list[str]) -> dict[str, int | No
     return {location_name: LOCATION_NAME_TO_ID[location_name] for location_name in location_names}
 
 
-def create_all_locations(world: APQuestWorld) -> None:
+def create_all_locations(world: SweetDefeatWorld) -> None:
     create_regular_locations(world)
     create_events(world)
 
 
-def create_regular_locations(world: APQuestWorld) -> None:
+def create_regular_locations(world: SweetDefeatWorld) -> None:
     # Finally, we need to put the Locations ("checks") into their regions.
     # Once again, before we do anything, we can grab our regions we created by using world.get_region()
     overworld = world.get_region("Overworld")
@@ -54,7 +54,7 @@ def create_regular_locations(world: APQuestWorld) -> None:
     right_room = world.get_region("Right Room")
 
     # One way to create locations is by just creating them directly via their constructor.
-    bottom_left_chest = APQuestLocation(
+    bottom_left_chest = SweetDefeatLocation(
         world.player, "Bottom Left Chest", world.location_name_to_id["Bottom Left Chest"], overworld
     )
 
@@ -68,22 +68,22 @@ def create_regular_locations(world: APQuestWorld) -> None:
     bottom_right_room_locations = get_location_names_with_ids(
         ["Bottom Right Room Left Chest", "Bottom Right Room Right Chest"]
     )
-    bottom_right_room.add_locations(bottom_right_room_locations, APQuestLocation)
+    bottom_right_room.add_locations(bottom_right_room_locations, SweetDefeatLocation)
 
     top_left_room_locations = get_location_names_with_ids(["Top Left Room Chest"])
-    top_left_room.add_locations(top_left_room_locations, APQuestLocation)
+    top_left_room.add_locations(top_left_room_locations, SweetDefeatLocation)
 
     right_room_locations = get_location_names_with_ids(["Right Room Enemy Drop"])
-    right_room.add_locations(right_room_locations, APQuestLocation)
+    right_room.add_locations(right_room_locations, SweetDefeatLocation)
 
     # Locations may be in different regions depending on the player's options.
     # In our case, the hammer option puts the Top Middle Chest into its own room called Top Middle Room.
     top_middle_room_locations = get_location_names_with_ids(["Top Middle Chest"])
     if world.options.hammer:
         top_middle_room = world.get_region("Top Middle Room")
-        top_middle_room.add_locations(top_middle_room_locations, APQuestLocation)
+        top_middle_room.add_locations(top_middle_room_locations, SweetDefeatLocation)
     else:
-        overworld.add_locations(top_middle_room_locations, APQuestLocation)
+        overworld.add_locations(top_middle_room_locations, SweetDefeatLocation)
 
     # Locations may exist only if the player enables certain options.
     # In our case, the extra_starting_chest option adds the Bottom Left Extra Chest location.
@@ -92,10 +92,11 @@ def create_regular_locations(world: APQuestWorld) -> None:
         # exist, it must still always be present in the world's location_name_to_id.
         # Whether the location actually exists in the seed is purely determined by whether we create and add it here.
         bottom_left_extra_chest = get_location_names_with_ids(["Bottom Left Extra Chest"])
-        overworld.add_locations(bottom_left_extra_chest, APQuestLocation)
+        overworld.add_locations(bottom_left_extra_chest, SweetDefeatLocation)
 
 
-def create_events(world: APQuestWorld) -> None:
+def create_events(world: SweetDefeatWorld) -> None:
+    pass
     # Sometimes, the player may perform in-game actions that allow them to progress which are not related to Items.
     # In our case, the player must press a button in the top left room to open the final boss door.
     # AP has something for this purpose: "Event locations" and "Event items".
@@ -103,34 +104,34 @@ def create_events(world: APQuestWorld) -> None:
     # It is treated during generation like any other location, but then it is discarded.
     # This location cannot be "sent" and its item cannot be "received", but the item can be used in logic rules.
     # Since we are creating more locations and adding them to regions, we need to grab those regions again first.
-    top_left_room = world.get_region("Top Left Room")
-    final_boss_room = world.get_region("Final Boss Room")
-
-    # One way to create an event is simply to use one of the normal methods of creating a location.
-    button_in_top_left_room = APQuestLocation(world.player, "Top Left Room Button", None, top_left_room)
-    top_left_room.locations.append(button_in_top_left_room)
-
-    # We then need to put an event item onto the location.
-    # An event item is an item whose code is "None" (same as the event location's address),
-    # and whose classification is "progression". Item creation will be discussed more in items.py.
-    # Note: Usually, items are created in world.create_items(), which for us happens in items.py.
-    # However, when the location of an item is known ahead of time (as is the case with an event location/item pair),
-    # it is common practice to create the item when creating the location.
-    # Since locations also have to be finalized after world.create_regions(), which runs before world.create_items(),
-    # we'll create both the event location and the event item in our locations.py code.
-    button_item = items.APQuestItem("Top Left Room Button Pressed", ItemClassification.progression, None, world.player)
-    button_in_top_left_room.place_locked_item(button_item)
-
-    # A way simpler way to do create an event location/item pair is by using the region.create_event helper.
-    # Luckily, we have another event we want to create: The Victory event.
-    # We will use this event to track whether the player can win the game.
-    # The Victory event is a completely optional abstraction - This will be discussed more in set_rules().
-    final_boss_room.add_event(
-        "Final Boss Defeated", "Victory", location_type=APQuestLocation, item_type=items.APQuestItem
-    )
-
-    # If you create all your regions and locations line-by-line like this,
-    # the length of your create_regions might get out of hand.
-    # Many worlds use more data-driven approaches using dataclasses or NamedTuples.
-    # However, it is worth understanding how the actual creation of regions and locations works,
-    # That way, we're not just mindlessly copy-pasting! :)
+    # top_left_room = world.get_region("Top Left Room")
+    # final_boss_room = world.get_region("Final Boss Room")
+    #
+    # # One way to create an event is simply to use one of the normal methods of creating a location.
+    # button_in_top_left_room = APQuestLocation(world.player, "Top Left Room Button", None, top_left_room)
+    # top_left_room.locations.append(button_in_top_left_room)
+    #
+    # # We then need to put an event item onto the location.
+    # # An event item is an item whose code is "None" (same as the event location's address),
+    # # and whose classification is "progression". Item creation will be discussed more in items.py.
+    # # Note: Usually, items are created in world.create_items(), which for us happens in items.py.
+    # # However, when the location of an item is known ahead of time (as is the case with an event location/item pair),
+    # # it is common practice to create the item when creating the location.
+    # # Since locations also have to be finalized after world.create_regions(), which runs before world.create_items(),
+    # # we'll create both the event location and the event item in our locations.py code.
+    # button_item = items.APQuestItem("Top Left Room Button Pressed", ItemClassification.progression, None, world.player)
+    # button_in_top_left_room.place_locked_item(button_item)
+    #
+    # # A way simpler way to do create an event location/item pair is by using the region.create_event helper.
+    # # Luckily, we have another event we want to create: The Victory event.
+    # # We will use this event to track whether the player can win the game.
+    # # The Victory event is a completely optional abstraction - This will be discussed more in set_rules().
+    # final_boss_room.add_event(
+    #     "Final Boss Defeated", "Victory", location_type=APQuestLocation, item_type=items.APQuestItem
+    # )
+    #
+    # # If you create all your regions and locations line-by-line like this,
+    # # the length of your create_regions might get out of hand.
+    # # Many worlds use more data-driven approaches using dataclasses or NamedTuples.
+    # # However, it is worth understanding how the actual creation of regions and locations works,
+    # # That way, we're not just mindlessly copy-pasting! :)
