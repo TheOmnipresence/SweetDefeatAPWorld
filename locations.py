84a00f019abd6d5e42503f0bf80d75e745368a28
TheOmnipresence <ItsTheOmnipresence@gmail.com>
2026-06-05 21:45:17 -0400

    locations

--- locations.py
+++ locations.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 
 from BaseClasses import ItemClassification, Location
 
@@ -9,19 +9,34 @@ from . import items
 if TYPE_CHECKING:
     from .world import SweetDefeatWorld
 
-# Every location must have a unique integer ID associated with it.
-# We will have a lookup from location name to ID here that, in world.py, we will import and bind to the world class.
-# Even if a location doesn't exist on specific options, it must be present in this lookup.
-LOCATION_NAME_TO_ID = {
-    "Top Left Room Chest": 1,
-    "Top Middle Chest": 2,
-    "Bottom Left Chest": 3,
-    "Bottom Left Extra Chest": 4,
-    "Bottom Right Room Left Chest": 5,
-    "Bottom Right Room Right Chest": 6,
-    # Location IDs don't need to be sequential, as long as they're unique and greater than 0.
-    "Right Room Enemy Drop": 10,
-}
+
+def map_to_dict(array: list, method: Callable) -> dict:
+    result = {}
+    for i in array:
+        result[i] = method(i)
+    return result
+
+
+def get_levels() -> list:
+    result = []
+    for level_type in LEVEL_TYPES:
+        for level in range(MAX_LEVELS):
+            result.append(level_type + " Level " + str(level))
+    return result
+
+
+MAX_LEVELS = 3
+
+LEVEL_TYPES = [
+    "Normal",
+    "Node",
+    "Rampage",
+    "Cool",
+]
+
+ALL_LEVELS = get_levels()
+
+LOCATION_NAME_TO_ID = map_to_dict(ALL_LEVELS, lambda e: ALL_LEVELS.index(e) + 1)
 
 
 # Each Location instance must correctly report the "game" it belongs to.
@@ -30,12 +45,6 @@ class SweetDefeatLocation(Location):
     game = "Sweet Defeat"
 
 
-# Let's make one more helper method before we begin actually creating locations.
-# Later on in the code, we'll want specific subsections of LOCATION_NAME_TO_ID.
-# To reduce the chance of copy-paste errors writing something like {"Chest": LOCATION_NAME_TO_ID["Chest"]},
-# let's make a helper method that takes a list of location names and returns them as a dict with their IDs.
-# Note: There is a minor typing quirk here. Some functions want location addresses to be an "int | None",
-# so while our function here only ever returns dict[str, int], we annotate it as dict[str, int | None].
 def get_location_names_with_ids(location_names: list[str]) -> dict[str, int | None]:
     return {location_name: LOCATION_NAME_TO_ID[location_name] for location_name in location_names}
 
@@ -46,53 +55,17 @@ def create_all_locations(world: SweetDefeatWorld) -> None:
 
 
 def create_regular_locations(world: SweetDefeatWorld) -> None:
-    # Finally, we need to put the Locations ("checks") into their regions.
-    # Once again, before we do anything, we can grab our regions we created by using world.get_region()
-    overworld = world.get_region("Overworld")
-    top_left_room = world.get_region("Top Left Room")
-    bottom_right_room = world.get_region("Bottom Right Room")
-    right_room = world.get_region("Right Room")
 
-    # One way to create locations is by just creating them directly via their constructor.
-    bottom_left_chest = SweetDefeatLocation(
-        world.player, "Bottom Left Chest", world.location_name_to_id["Bottom Left Chest"], overworld
-    )
-
-    # You can then add them to the region.
-    overworld.locations.append(bottom_left_chest)
-
-    # A simpler way to do this is by using the region.add_locations helper.
-    # For this, you need to have a dict of location names to their IDs (i.e. a subset of location_name_to_id)
-    # Aha! So that's why we made that "get_location_names_with_ids" helper method earlier.
-    # You also need to pass your overridden Location class.
-    bottom_right_room_locations = get_location_names_with_ids(
-        ["Bottom Right Room Left Chest", "Bottom Right Room Right Chest"]
-    )
-    bottom_right_room.add_locations(bottom_right_room_locations, SweetDefeatLocation)
-
-    top_left_room_locations = get_location_names_with_ids(["Top Left Room Chest"])
-    top_left_room.add_locations(top_left_room_locations, SweetDefeatLocation)
-
-    right_room_locations = get_location_names_with_ids(["Right Room Enemy Drop"])
-    right_room.add_locations(right_room_locations, SweetDefeatLocation)
-
-    # Locations may be in different regions depending on the player's options.
-    # In our case, the hammer option puts the Top Middle Chest into its own room called Top Middle Room.
-    top_middle_room_locations = get_location_names_with_ids(["Top Middle Chest"])
-    if world.options.hammer:
-        top_middle_room = world.get_region("Top Middle Room")
-        top_middle_room.add_locations(top_middle_room_locations, SweetDefeatLocation)
-    else:
-        overworld.add_locations(top_middle_room_locations, SweetDefeatLocation)
+    world.get_region("Game").add_locations(get_location_names_with_ids(ALL_LEVELS))
 
     # Locations may exist only if the player enables certain options.
     # In our case, the extra_starting_chest option adds the Bottom Left Extra Chest location.
-    if world.options.extra_starting_chest:
-        # Once again, it is important to stress that even though the Bottom Left Extra Chest location doesn't always
-        # exist, it must still always be present in the world's location_name_to_id.
-        # Whether the location actually exists in the seed is purely determined by whether we create and add it here.
-        bottom_left_extra_chest = get_location_names_with_ids(["Bottom Left Extra Chest"])
-        overworld.add_locations(bottom_left_extra_chest, SweetDefeatLocation)
+    # if world.options.extra_starting_chest:
+    #     # Once again, it is important to stress that even though the Bottom Left Extra Chest location doesn't always
+    #     # exist, it must still always be present in the world's location_name_to_id.
+    #     # Whether the location actually exists in the seed is purely determined by whether we create and add it here.
+    #     bottom_left_extra_chest = get_location_names_with_ids(["Bottom Left Extra Chest"])
+    #     overworld.add_locations(bottom_left_extra_chest, SweetDefeatLocation)
 
 
 def create_events(world: SweetDefeatWorld) -> None:
