ea77e2d86ffe4a125bdf55d3414141382719a20e
TheOmnipresence <ItsTheOmnipresence@gmail.com>
2026-06-08 18:11:15 -0400

    yet another fix to deathlink

--- world.py
+++ world.py
@@ -81,5 +81,5 @@ class SweetDefeatWorld(World):
     def fill_slot_data(self) -> Mapping[str, Any]:
         # If you need access to the player's chosen options on the client side, there is a helper for that.
         return self.options.as_dict(
-            "death_link", "deathlink_amnesty"
+            "death_link", "death_link_amnesty"
         )
