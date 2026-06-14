d35a20725f2315e4c0ccbb97e676ede28039dc39
TheOmnipresence <ItsTheOmnipresence@gmail.com>
2026-06-14 15:39:08 -0400

    remove comments

--- web_world.py
+++ web_world.py
@@ -4,14 +4,9 @@ from worlds.AutoWorld import WebWorld
 from .options import option_groups, option_presets
 
 
-# For our game to display correctly on the website, we need to define a WebWorld subclass.
 class SweetDefeatWebWorld(WebWorld):
-    # We need to override the "game" field of the WebWorld superclass.
-    # This must be the same string as the regular World class.
     game = "Sweet Defeat"
 
-    # Your game pages will have a visual theme (affecting e.g. the background image).
-    # You can choose between dirt, grass, grassFlowers, ice, jungle, ocean, partyTime, and stone.
     theme = "grass"
 
     setup_en = Tutorial(
--- world.py
+++ world.py
@@ -1,27 +1,12 @@
 from collections.abc import Mapping
 from typing import Any
 
-# Imports of base Archipelago modules must be absolute.
 from worlds.AutoWorld import World
 
-# Imports of your world's files must be relative.
 from . import items, locations, regions, rules, web_world
-from . import options as sweet_defeat_options  # rename due to a name conflict with World.options
-
-# APQuest will go through all the parts of the world api one step at a time,
-# with many examples and comments across multiple files.
-# If you'd rather read one continuous document, or just like reading multiple sources,
-# we also have this document specifying the entire world api:
-# https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/world%20api.md
+from . import options as sweet_defeat_options
 
 
-# The world class is the heart and soul of an apworld implementation.
-# It holds all the data and functions required to build the world and submit it to the multiworld generator.
-# You could have all your world code in just this one class, but for readability and better structure,
-# it is common to split up world functionality into multiple files.
-# This implementation in particular has the following additional files, each covering one topic:
-# regions.py, locations.py, rules.py, items.py, options.py and web_world.py.
-# It is recommended that you read these in that specific order, then come back to the world class.
 class SweetDefeatWorld(World):
     """
     Sweet Defeat is a fast-paced speedrunning platformer designed around a melting popsicle.
